2daa7c765ecb0169e7a2402e8aadef8145530080
AlokRai378 <rai281442@gmail.com>
2026-09-06 19:34:59 +0530

    Create EvenOdd.java

---
+++ EvenOdd.java
@@ -0,0 +1,11 @@
+class EvenOdd {
+        public static void main (string[] args) {
+        int n = 15;
+        if (n % 2 ==0) {  
+       System.out.println( "Even");
+        } else {
+          System.out.println("Odd");
+        }
+        }
+}
+`
