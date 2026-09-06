619d9d4b00d47a881ff9d0f0f5ee56576179c5c0
AlokRai378 <rai281442@gmail.com>
2026-09-06 11:08:32 +0530

    Create Addition.java

---
+++ Addition.java
@@ -0,0 +1,11 @@
+import.java.unitl.Scanner;
+class Addition {
+  public static void main(String[]args) {
+  Scanner sc = new Scanner(system.ln);
+  System.out.print("Enter first number: ");
+    int a= sc.nextlnt();
+    System.out.print("Enter second number:");
+     int b =sc.nextint();
+     System.out.println("Sum =" +(a+b));
+  }
+}  
